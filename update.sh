3abda96d3e8989b600ee83cd2a9a5fc35876264f
Thyago Leal <thyagoleal@yahoo.com>
2021-02-09 14:33:35 -0300

    created deploy script

---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "Missing commit message!"
+    exit 1
+
+else 
+    git add -A
+    git commit -am "$1"
+    git push
+
+    hugo
+
+    cd public
+    git add -add -A 
+    git commit -am "$1"
+
+    cd ..
+
+    git push -u origin main --recurse-submodules=on-demand
+
+echo "Done!\n"
+exit 0
